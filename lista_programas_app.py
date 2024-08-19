ee4f349184de0304d7aff44f26818db0c7f9ddd2
Xandy77 <domirops@yahoo.com.br>
2024-08-19 18:03:09 -0300

    Versão 1.0

---
+++ lista_programas_app.py
@@ -0,0 +1,11 @@
+# importa biblioteca
+import winapps
+
+# exibe lista de programas
+for item in winapps.list_installed():
+    print(f'Programa: {item.name}.')
+    print(f'Versão: {item.version}.')
+    print(f'Data da instalação: {item.install_date}.')
+    print(f'Data da publicação: {item.publisher}.')
+    print(f'Local de desinstalação: {item.uninstall_string}.')
+    print('-'*50)
